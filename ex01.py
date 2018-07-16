76a89924100c01f9ec0258f94fd3672a41515a00
jiabin713 <jiabin713@gmail.com>
2018-07-16 14:55:51 +0800

    ex01

--- ex01.py
+++ ex01.py
@@ -1,3 +1,5 @@
+#ex01: A Good First Program
+
 print("Hello World!") 
 print("Hello Again")
 print("I like typing this.")
@@ -5,3 +7,7 @@ print("This is fun.")
 print('Yap! Printing.')
 print("I'd much rather you 'not'.")
 print('I "said" do not touch this.')
+
+#ex01: Study Drills
+print('another line')
+#print('put a # character')
