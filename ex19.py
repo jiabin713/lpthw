484d697ea2fb1962650334034601a09d7782c4c1
jiabin713 <jiabin713@gmail.com>
2018-07-17 15:08:07 +0800

    ex19

--- ex19.py
+++ ex19.py
@@ -1,10 +1,12 @@
 #ex19: Functions and Variables
 
+
 def cheese_and_crackers(cheese_count, boxes_of_crackers):
-  print(f"You have {cheese_count} cheeses!")
-  print(f"You have {boxes_of_crackers} boxes of crackers!")
-  print("Man that's enough for a party!")
-  print("Get a blanket.\n")
+    print(f"You have {cheese_count} cheeses!")
+    print(f"You have {boxes_of_crackers} boxes of crackers!")
+    print("Man that's enough for a party!")
+    print("Get a blanket.\n")
+
 
 print("We can just give the function numbers directly:")
 cheese_and_crackers(20, 30)
